d55fe4d00d9af010ba901c4438d553f114928cb4
root <Part4@github.com>
2023-11-23 02:50:12 -0800

    add print_modulo

---
+++ print_modulo.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_modulo - Prototypes
+ * @args: arguments
+ * Return: 1
+ */
+
+int print_modulo(va_list args)
+{
+	(void)args;
+	_putchar('%');
+	return (1);
+}
