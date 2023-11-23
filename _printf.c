641146b7595770001932488906926fe5a3d489ca
root <Part4@github.com>
2023-11-23 04:43:12 -0800

    add _printf

--- _printf.c
+++ _printf.c
@@ -11,28 +11,41 @@ int _printf(const char *format, ...)
 {
 	int i = 0;
 	int count = 0;
+	int count_fun;
 	va_list args;
 
 	va_start(args, format);
-
-	if (format == NULL)
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-
-	while (format != NULL  && format[i] != '\0')
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	while (format[i])
 	{
+		count_fun = 0;
 		if (format[i] == '%')
 		{
-			if (!format[i + 1] == '\0')
-				return (-1);
+			if (!format[i + 1] || (format[i + 1] == ' ' && !format[i + 2]))
+			{
+				count = -1;
+				break;
+			}
+			count_fun += function(format[i + 1], args);
 
-			count += function(format[i + 1], args);
-			i += 2;
+			if (count_fun == 0)
+				count += _putchar(format[i + 1]);
+
+			if (count_fun == -1)
+				count = -1;
+			i++;
 		}
 		else
 		{
-			count += _putchar(format[i]);
-			i++;
+			(count == -1) ? (_putchar(format[i])) : (count += _putchar(format[i]));
 		}
+		i++;
+
+		if (count != -1)
+			count += count_fun;
 	}
 	va_end(args);
 	return (count);
