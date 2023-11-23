6618d88cae842ffcd5604a6570b9d67b217bbd8d
root <Part4@github.com>
2023-11-23 02:51:23 -0800

    print_string

---
+++ print_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdarg.h>
+
+/**
+ * print_string - Prototypes
+ * @args: arguments
+ * Return: totalCharacters
+ */
+
+int print_string(va_list args)
+{
+	int i;
+	int totalCharacters = 0;
+	char *str = va_arg(args, char *);
+
+	if (!str)
+		str = "NULL";
+
+	if (str[0] == '\0')
+		return (-1);
+
+	for (i = 0; str[i] != '\0'; i++)
+		totalCharacters += _putchar(str[i]);
+
+	return (totalCharacters);
+}
